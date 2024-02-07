edfca3471d625a4bc50bbec6da2ce77e66ad47b7
Pramesh Basnet <bpramesh742@gmail.com>
2024-02-07 15:38:50 +0545

    change herosection

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+    return (
+        <section>
+            <div className=''>
+                <h1>Create a website without limits</h1>
+                <p>What kind of website would you like to create ?</p>
+            </div>
+        </section>
+    )
 }
 
 export default Hero
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Background } from "../Components/Background";
+import Hero from "../Components/Hero";
 
 export const Home = () => {
   return (
