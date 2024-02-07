c17142b85f9344c80c2c2bd2ec11ff3b3895cf64
sushanttsharma0011 <135987312+sushanttsharma0011@users.noreply.github.com>
2024-02-08 00:06:07 +0545

    changes

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Buttons = () => {
   return (
     <div>
-      <div className="absolute right-20 top-7 shadow-sm">
+      <div className=" hidden absolute xl:right-20 right-3 top-6 shadow-sm">
         <button className="bg-[#2b328a] hover:bg-[#525bd4] font-Montserrat font-medium hover:font-bold  text-white hover:text-primary p-3 rounded-full w-40">
           Get Started
         </button>
