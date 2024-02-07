9a42d8e88dd927807e18895098f554b27b83ee14
sushanttsharma0011 <135987312+sushanttsharma0011@users.noreply.github.com>
2024-02-07 23:20:02 +0545

    changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
     <section>
       {" "}
-      {/*the is a backgroung color:bg-accent*/}
+      {/*the is a backgroung color*/}
       <div className="h-max w-full bg-gradient-to-tr from-[#5a6ce6]  via-[#8a92b8]  % to-[#26338A]">
         <div className="p-28">
           <div className="text-center">
@@ -27,63 +27,39 @@ const Hero = () => {
           </div>
           <div className="">
             <div className="flex justify-center">
-              <div className="grid grid-cols-4 items-center gap-3">
-                <a
-                  href="#"
-                  className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-2xl cursor-pointer hover:bg-gray-600"
-                >
+              <div className="grid grid-cols-4 items-center   gap-3">
+                <div className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-2xl">
                   <FaStore />
                   Online Store
-                </a>
+                </div>
 
-                <a
-                  href="#"
-                  className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1"
-                >
+                <div className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1">
                   <FaBriefcase />
                   Portfolio
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1 "
-                >
+                </div>
+                <div className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1 ">
                   <FaBloggerB />
                   Blog
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1 "
-                >
+                </div>
+                <div className="flex items-center justify-center p-2 gap-3 rounded-full  border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1 ">
                   <BiSolidBriefcaseAlt />
-                  Consultant
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1"
-                >
+                  Consultandiv
+                </div>
+                <div className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1">
                   <SlCalender />
                   Business
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1"
-                >
+                </div>
+                <div className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1">
                   <FaHotel />
                   Restaurant
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1"
-                >
+                </div>
+                <div className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1">
                   <MdEmojiEvents />
                   Event
-                </a>
-                <a
-                  href="#"
-                  className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1"
-                >
+                </div>
+                <div className="flex items-center justify-center p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer sm:text-[20px] sm:justify-around sm:p-1">
                   ...Other
-                </a>
+                </div>
               </div>
             </div>
             <div className="flex justify-center mt-8">
@@ -93,11 +69,21 @@ const Hero = () => {
               </button>
             </div>
             <p className="text-center mt-4">Trusted by 20+ users worldwide.</p>
-            <div className="mt-10">
+            <div className=" container mt-10 mx-auto grid grid-cols-3 gap-6">
               <img
                 src="https://imgs.search.brave.com/308RhKwr_nnai4XuekUhKahVcs9FcNdw-bP9eDQkyJU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9idWZm/ZXIuY29tL2xpYnJh/cnkvY29udGVudC9p/bWFnZXMvMjAyMy8x/MC9mcmVlLWltYWdl/cy1mb3ItY29tbWVy/Y2lhbC11c2UtLTE3/LS5wbmc"
                 alt="Your Image"
-                className="w-full h-auto rounded-lg"
+                className="w-full h-full rounded-lg"
+              />
+              <img
+                src="https://imgs.search.brave.com/308RhKwr_nnai4XuekUhKahVcs9FcNdw-bP9eDQkyJU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9idWZm/ZXIuY29tL2xpYnJh/cnkvY29udGVudC9p/bWFnZXMvMjAyMy8x/MC9mcmVlLWltYWdl/cy1mb3ItY29tbWVy/Y2lhbC11c2UtLTE3/LS5wbmc"
+                alt="Your Image"
+                className="w-full h-full rounded-lg"
+              />
+              <img
+                src="https://imgs.search.brave.com/308RhKwr_nnai4XuekUhKahVcs9FcNdw-bP9eDQkyJU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9idWZm/ZXIuY29tL2xpYnJh/cnkvY29udGVudC9p/bWFnZXMvMjAyMy8x/MC9mcmVlLWltYWdl/cy1mb3ItY29tbWVy/Y2lhbC11c2UtLTE3/LS5wbmc"
+                alt="Your Image"
+                className="w-full h-full rounded-lg"
               />
             </div>
           </div>
@@ -134,7 +120,7 @@ const Hero = () => {
             </p>
           </div>
         </div>
-        <button className=" mt-16 p-2 gap-3 rounded-full border border-black text-[15px] cursor-pointer items-center justify-center sm:text-[20px] sm:justify-around ">
+        <button className=" mt-16 p-2 bg-primary text-white px-6 gap-3 rounded-full border border-black text-[15px]   items-center justify-center sm:text-[20px] sm:justify-around ">
           Get Started
         </button>
       </div>
