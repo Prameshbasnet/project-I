4d61cf34eb2e7af093368e8ca32ff818fe65a8da
sushanttsharma0011 <135987312+sushanttsharma0011@users.noreply.github.com>
2024-02-07 16:32:08 +0545

    changes navabr

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,35 +1,33 @@
-import React from 'react'
+import React from "react";
 import { FaStore, FaBriefcase } from "react-icons/fa";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-    return (
-        <section>
-            <div className='my-10'>
-                <h1 className='text-center text-6xl my-7'>Create a website without limits</h1>
-                <p className='text-center my-5'>What kind of website would you like to create ?</p>
-                <div className='grid grid-cols-4'>
-                    <a href='#'>
-                        <FaStore />
-                        Online Store
-                    </a>
-                    <a href="#">
-                        <FaBriefcase />
-                        Portfolio
-                    </a>
-                    <a href="#">
-                        Blog
-                    </a>
-                    <a href="#">
-                        Consultant
-                    </a>
-                    <a href="#">
-                        Services Business
-                    </a>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section>
+      <div className="my-10">
+        <h1 className="text-center text-6xl my-7">
+          Create a website without limits
+        </h1>
+        <p className="text-center my-5">
+          What kind of website would you like to create ?
+        </p>
+        <div className="grid grid-cols-4">
+          <a href="#">
+            <FaStore />
+            Online Store
+          </a>
+          <a href="#">
+            <FaBriefcase />
+            Portfolio
+          </a>
+          <a href="#">Blog</a>
+          <a href="#">Consultant</a>
+          <a href="#">Services Business</a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // logo
 
@@ -7,167 +7,167 @@ import Logo2 from "../assets/logo2.png";
 
 // navbar data
 
-const NavbarData = [
-  {
-    id: 1,
-    name: "Inaguration",
-    Subname: [
-      {
-        title: "Web Design",
-        description: "Create amazing designs and develop your brand on DnD",
-      },
-      {
-        title: "Web Design",
-        description: "Create amazing designs and develop your brand on DnD",
-      },
-      {
-        title: "Web Templates",
-        description:
-          "Explore 200+ designer made templates & start with the right for you",
-      },
-      {
-        title: "Advanced Web Development",
-        description:
-          "Build your next project with our advanced web development services",
-      },
-      {
-        title: "Mobile App",
-        description: "Build customise and manage your website on the go.",
-      },
-      {
-        title: "Hire a Professional",
-        description:
-          "Hire a professional web designer or developer for your next project",
-      },
-      {
-        title: "WEBSITE ESSENTIALS",
-        subtitle: [
-          {
-            title: "Custom Domain Name",
-          },
-          {
-            title: "Secure Web Hosting",
-          },
-          {
-            title: "Logo Maker",
-          },
-          {
-            title: "SEO Tools",
-          },
-        ],
-      },
-      {
-        title: "Become a Partner",
-        description: "Gain partner benefits & scale your business",
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: "Business",
-    Subname: [
-      {
-        title: "Service Business",
-        description:
-          "Manage bookings, payments, staff and clients with our scheduling solution",
-      },
-      {
-        title: "eCommerce Websites",
-        description:
-          "Self products and mangage fulfillment from one eCommerce platform",
-      },
-      {
-        title: "Blog Website",
-        description:
-          "Share ideas with your audience and drive traffic to your website",
-      },
-      {
-        title: "Resturants",
-        description: "Display your menu snd take orders & reservations online",
-      },
-      {
-        title: "Mobile App",
-        description: "Build customise and manage your website on the go.",
-      },
-      {
-        title: "Fitness",
-        description:
-          "Offer classes and manage your clients, bookings and payments online",
-      },
-      {
-        title: "Tools & Features",
-        subtitle: [
-          {
-            title: "Online Payments",
-          },
-          {
-            title: "Multichannel Sales",
-          },
-          {
-            title: "Point of sale",
-          },
-          {
-            title: "Analytics & Reports",
-          },
-          {
-            title: "Business Tools",
-          },
-          {
-            title: "Event Creation",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: "Assets",
-    Subname: [
-      {
-        title: "All Features",
-        description:
-          "Find everything you need to build your site and run your business",
-      },
-      {
-        title: "Web Design Inspiration",
-        description:
-          "Get insired by unique design & powerful websites built on Dnd",
-      },
-      {
-        title: "DnD Learn",
-        description:
-          "Take video courses & lessons and gain the skills you need to succeed online",
-      },
-      {
-        title: "DnD Blog",
-        description:
-          "Get business tips, online marketting guidance, and the latest websites trends",
-      },
-    ],
-  },
-  {
-    id: 4,
-    name: "Pricing",
-  },
-  {
-    id: 5,
-    name: "Support",
-  },
-  {
-    id: 6,
-    name: "DnD Studio",
-  },
-  {
-    id: 7,
-    name: "Enterprise",
-  },
-];
-
 export const NavBar = () => {
+  const [NavbarData, setNavabarData] = useState([
+    {
+      id: 1,
+      name: "Inaguration",
+      Subname: [
+        {
+          title: "Web Design",
+          description: "Create amazing designs and develop your brand on DnD",
+        },
+        {
+          title: "Web Design",
+          description: "Create amazing designs and develop your brand on DnD",
+        },
+        {
+          title: "Web Templates",
+          description:
+            "Explore 200+ designer made templates & start with the right for you",
+        },
+        {
+          title: "Advanced Web Development",
+          description:
+            "Build your next project with our advanced web development services",
+        },
+        {
+          title: "Mobile App",
+          description: "Build customise and manage your website on the go.",
+        },
+        {
+          title: "Hire a Professional",
+          description:
+            "Hire a professional web designer or developer for your next project",
+        },
+        {
+          title: "WEBSITE ESSENTIALS",
+          subtitle: [
+            {
+              title: "Custom Domain Name",
+            },
+            {
+              title: "Secure Web Hosting",
+            },
+            {
+              title: "Logo Maker",
+            },
+            {
+              title: "SEO Tools",
+            },
+          ],
+        },
+        {
+          title: "Become a Partner",
+          description: "Gain partner benefits & scale your business",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "Business",
+      Subname: [
+        {
+          title: "Service Business",
+          description:
+            "Manage bookings, payments, staff and clients with our scheduling solution",
+        },
+        {
+          title: "eCommerce Websites",
+          description:
+            "Self products and mangage fulfillment from one eCommerce platform",
+        },
+        {
+          title: "Blog Website",
+          description:
+            "Share ideas with your audience and drive traffic to your website",
+        },
+        {
+          title: "Resturants",
+          description:
+            "Display your menu snd take orders & reservations online",
+        },
+        {
+          title: "Mobile App",
+          description: "Build customise and manage your website on the go.",
+        },
+        {
+          title: "Fitness",
+          description:
+            "Offer classes and manage your clients, bookings and payments online",
+        },
+        {
+          title: "Tools & Features",
+          subtitle: [
+            {
+              title: "Online Payments",
+            },
+            {
+              title: "Multichannel Sales",
+            },
+            {
+              title: "Point of sale",
+            },
+            {
+              title: "Analytics & Reports",
+            },
+            {
+              title: "Business Tools",
+            },
+            {
+              title: "Event Creation",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: "Assets",
+      Subname: [
+        {
+          title: "All Features",
+          description:
+            "Find everything you need to build your site and run your business",
+        },
+        {
+          title: "Web Design Inspiration",
+          description:
+            "Get insired by unique design & powerful websites built on Dnd",
+        },
+        {
+          title: "DnD Learn",
+          description:
+            "Take video courses & lessons and gain the skills you need to succeed online",
+        },
+        {
+          title: "DnD Blog",
+          description:
+            "Get business tips, online marketting guidance, and the latest websites trends",
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "Pricing",
+    },
+    {
+      id: 5,
+      name: "Support",
+    },
+    {
+      id: 6,
+      name: "DnD Studio",
+    },
+    {
+      id: 7,
+      name: "Enterprise",
+    },
+  ]);
   return (
-    <div className=" hover:bg-white transition-all duration-300 border-b border-white/30 group">
-      <div className="container mx-20  h-16 w-full">
-        <div className="py-4">
+    <div className=" hover:bg-white transition-all duration-300 border-b border-white/30 group pt-4  w-full">
+      <div className="container mx-20  flex  items-center  w-full">
+        <div className="mr-16">
           <img
             className=" hidden group-hover:flex "
             src={Logo}
@@ -182,6 +182,45 @@ export const NavBar = () => {
             height={110}
             width={110}
           />
+        </div>
+        <div className="hidden lg:flex w-full">
+          <ul className=" flex text-white w-max group-hover:text-primary  font-Montserrat text-xl  gap-x-10">
+            {NavbarData?.map((element) => (
+              <li
+                onMouseOver={() => {
+                  setNavabarData((prev) =>
+                    prev.map((item) =>
+                      item.id === element.id
+                        ? { ...item, active: true }
+                        : { ...item, active: false }
+                    )
+                  );
+                }}
+                onMouseOut={() => {
+                  setNavabarData((prev) =>
+                    prev.map((item) => ({ ...item, active: false }))
+                  );
+                }}
+                className=""
+                key={element.id}
+              >
+                <a href="#">{element.name}</a>
+                {element.Subname && element.active && (
+                  <div>
+                    <div>
+                      <ul className=" hidden hover:block ">
+                        {element?.Subname?.map((subdata) => (
+                          <li key={subdata.title}>
+                            <a href="#">{subdata.title}</a>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  </div>
+                )}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
       ;
