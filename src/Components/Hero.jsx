89f5d352309a353dbed575e9d7ed3b04db92229b
Pramesh Basnet <bpramesh742@gmail.com>
2024-02-07 16:56:14 +0545

    hh

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             <div className='p-28'>
                 <div className=''>
                     <h1 className='text-center text-6xl'>Create a website without limits</h1>
-                    <p className='text-center my-5 text-xl'>What kind of website would you like to create ?</p>
+                    <p className='text-center my-8 text-xl'>What kind of website would you like to create ?</p>
                     <div className='flex justify-center'>
                         <div className='grid grid-cols-4 items-center gap-3'>
                             <a className='flex items-center justify-center p-2 gap-3 rounded-full border border-black text-2xl cursor-pointer'>
